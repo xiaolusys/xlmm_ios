199b8bfd2cdf876c6b28504e818a3e883224d407
Mona <2444946401@qq.com>
2015-07-29 15:00:27 +0800

    Initial Commit

---
+++ XLMM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XLMM
+//
+//  Created by younishijie on 15/7/29.
+//  Copyright (c) 2015年 上海己美. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
