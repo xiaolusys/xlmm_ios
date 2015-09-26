4dbf07c73a2c9926ff6c4ec049e5738acdd881a3
Mona <2444946401@qq.com>
2015-09-26 11:43:24 +0800

    微信登录

--- XLMM/EnterViewController.m
+++ XLMM/EnterViewController.m
@@ -10,9 +10,15 @@
 #import "LogInViewController.h"
 #import "MMClass.h"
 #import "WXApi.h"
+#import "WXLoginController.h"
+#import <CommonCrypto/CommonDigest.h>
+#import "NSString+Encrypto.h"
+
+#define SECRET @"3c7b4e3eb5ae4cfb132b2ac060a872ee"
 
 @interface EnterViewController ()<WXApiDelegate>{
     NSTimer *theTimer;
+    NSMutableString *randomstring;
     
     
     
@@ -44,26 +50,165 @@
     [self setInfo];
     NSNotificationCenter * notificationCenter = [ NSNotificationCenter defaultCenter];
     [notificationCenter addObserver: self selector: @selector (update:) name: @"login" object: nil ];
+    NSString *strings = @"noncestr=1442995986abcdef&secret=3c7b4e3eb5ae4c&timestamp=1442995986";
     
+    NSLog(@"%@", [strings sha1]);
+    if ([[strings sha1] isEqualToString:@"39ae931c59394c9b4b0973b3902956f63a35c21e"]) {
+        NSLog(@"****一样的\n");
+    }
     if ([WXApi isWXAppInstalled]) {
         NSLog(@"安装了微信");
         
         self.wxButton.hidden = NO;
-
+        
     }
     else{
         NSLog(@"没有安装微信");
         
         self.wxButton.hidden = YES;
-
+        
     }
+    // NSLog(@"randomArray = %@", [self randomArray]);
+    randomstring = [[NSMutableString alloc] init];
+    
+    
+    
     
 }
 
 
+/*
+ 
+ 
+ 
+ 1442999252RwOO7Qb70y
+ 1442999240QbaUmMxKys
+ 14429992191wXjOfh0vf
+ 
+ XPoAc9iDonWDsSdd8SL1
+ hxnrW2q8bNW3FGq7CuTY
+ Fq7OEkL7WZ0KhcSkCfud
+ 
+ 
+ 
+ */
+
+
 - (void)update:(NSNotificationCenter *)notification{
     NSLog(@"微信一键登录成功， 请您绑定手机号");
-    [self.navigationController popViewControllerAnimated:YES];
+    
+    
+    
+    NSDictionary *dic = [[NSUserDefaults standardUserDefaults]objectForKey:@"userInfo"];
+    NSLog(@"用户信息 = %@", dic);
+    
+    
+    
+    //微信登录 hash算法。。。。
+    
+    
+    
+    
+    NSArray *randomArray = [self randomArray];
+    unsigned long count = (unsigned long)randomArray.count;
+    NSLog(@"count = %lu", count);
+    int index = 0;
+    
+    NSString *timeSp = [NSString stringWithFormat:@"%ld", (long)[[NSDate date] timeIntervalSince1970]];
+    NSLog(@"timeSp:%@",timeSp);
+    
+    long time = [timeSp integerValue];
+    NSLog(@"time = %ld", (long)time);
+    
+    for (int i = 0; i<8; i++) {
+        index = arc4random()%count;
+        // NSLog(@"index = %d", index);
+        NSString *string = [randomArray objectAtIndex:index];
+        [randomstring appendString:string];
+    }
+    NSLog(@"%@%@",timeSp ,randomstring);
+    
+    
+    //    NSString *secret = @"3c7b4e3eb5ae4c";
+    NSString *noncestr = [NSString stringWithFormat:@"%@%@", timeSp, randomstring];
+    
+    
+    
+    //获得参数，升序排列
+    
+    
+    
+    
+    
+    
+    NSString* sign_params = [NSString stringWithFormat:@"noncestr=%@&secret=%@&timestamp=%@",noncestr, SECRET,timeSp];
+    
+    
+    
+    
+    NSLog(@"1.————》%@", sign_params);
+    
+    NSString *sign = [sign_params sha1];
+    NSString *dict;
+    
+    NSLog(@"sign = %@", sign);
+    
+    
+    //http://m.xiaolu.so/rest/v1/register/wxapp_login
+    NSString *urlString = [NSString stringWithFormat:@"%@/rest/v1/register/wxapp_login?noncestr=%@&timestamp=%@&sign=%@", Root_URL,noncestr, timeSp, sign];
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSLog(@"urlString = %@", urlString);
+    
+    NSMutableURLRequest * postRequest=[NSMutableURLRequest requestWithURL:url];
+    
+    dict = [NSString stringWithFormat:@"headimgurl=%@&nickname=%@&openid=%@&unionid=%@", [dic objectForKey:@"headimgurl"], [dic objectForKey:@"nickname"],[dic objectForKey:@"openid"],[dic objectForKey:@"unionid"]];
+    
+    NSLog(@"params = %@", dict);
+    NSData *data = [dict dataUsingEncoding:NSUTF8StringEncoding];
+    [postRequest setHTTPBody:data];
+    [postRequest setHTTPMethod:@"POST"];
+    [postRequest setValue:@"application/json; charset=utf-8" forHTTPHeaderField:@"Content-Type"];
+    
+    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+    //  [self showAlertWait];
+    [NSURLConnection sendAsynchronousRequest:postRequest queue:queue completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+        
+        NSHTTPURLResponse* httpResponse = (NSHTTPURLResponse*)response;
+        NSLog(@"response = %@", httpResponse);
+        NSString *str = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        
+        NSDictionary *dictionary = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
+        NSLog(@"dictionary = %@", dictionary);
+        
+        NSLog(@"dataString = %@", str);
+        
+        
+        
+        //提示用户输入手机号和密码：
+        
+        if ([[[dictionary objectForKey:@"info"] objectForKey:@"mobile"] isEqualToString:@""]) {
+            NSLog(@"未绑定手机号码");
+            WXLoginController *wxloginVC = [[WXLoginController alloc]  initWithNibName:@"WXLoginController" bundle:nil];
+            wxloginVC.userInfo = dic;
+            [self.navigationController pushViewController:wxloginVC animated:YES];
+            
+        } else {
+            NSLog(@"已绑定手机号码");
+            [self.navigationController popViewControllerAnimated:YES];
+            
+        }
+    }];
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     NSUserDefaults *userdefaults = [NSUserDefaults standardUserDefaults];
     [userdefaults setBool:YES forKey:@"login"];
@@ -73,6 +218,35 @@
     NSNotificationCenter * notificationCenter = [ NSNotificationCenter defaultCenter];
     [notificationCenter removeObserver:self name:@"login" object:nil];
 }
+
+-(NSString *)toLower:(NSString *)str{
+    for (NSInteger i=0; i<str.length; i++) {
+        if ([str characterAtIndex:i]>='A'&[str characterAtIndex:i]<='Z') {
+            //A  65  a  97
+            char  temp=[str characterAtIndex:i]+32;
+            NSRange range=NSMakeRange(i, 1);
+            str=[str stringByReplacingCharactersInRange:range withString:[NSString stringWithFormat:@"%c",temp]];
+        }
+    }
+    return str;
+}
+
+//- (NSString*) sha1
+//{
+//    const charchar *cstr = [self cStringUsingEncoding:NSUTF8StringEncoding];
+//    NSData *data = [NSData dataWithBytes:cstr length:self.length];
+//
+//    uint8_t digest[CC_SHA1_DIGEST_LENGTH];
+//
+//    CC_SHA1(data.bytes, data.length, digest);
+//
+//    NSMutableString* output = [NSMutableString stringWithCapacity:CC_SHA1_DIGEST_LENGTH * 2];
+//
+//    for(int i = 0; i < CC_SHA1_DIGEST_LENGTH; i++)
+//        [output appendFormat:@"%02x", digest[i]];
+//
+//    return output;
+//}
 - (void)setInfo{
     
     UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 200, 44)];
@@ -94,6 +268,29 @@
     
 }
 
+- (NSArray *)randomArray{
+    NSMutableArray *mutable = [[NSMutableArray alloc] initWithCapacity:62];
+    
+    for (int i = 0; i<10; i++) {
+        // NSLog(@"%d", i);
+        NSString *string = [NSString stringWithFormat:@"%d",i];
+        [mutable addObject:string];
+    }
+    for (char i = 'a'; i<='z'; i++) {
+        // NSLog(@"%c", i);
+        NSString *string = [NSString stringWithFormat:@"%c", i];
+        
+        [mutable addObject:string];
+    }
+    NSArray *array = [NSArray arrayWithArray:mutable];
+    return array;
+    
+    
+}
+
+//  NSArray
+
+//NSUInteger
 
 
 - (void)backBtnClicked:(UIButton *)button{
@@ -105,14 +302,14 @@
 }
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 - (IBAction)mmLogin:(id)sender {
     LogInViewController *loginVC = [[LogInViewController alloc] initWithNibName:@"LogInViewController" bundle:nil];
@@ -125,7 +322,7 @@
     NSLog(@"微信登录");
     
     [self sendAuthRequest];
-
+    
 }
 
 
@@ -134,7 +331,7 @@
 {
     SendAuthReq* req =[[SendAuthReq alloc ] init];
     req.scope = @"snsapi_userinfo,snsapi_base";
-    req.state = @"123" ;
+    req.state = @"xiaolu" ;
     
     NSLog(@"req = %@", req);
     [WXApi sendReq:req];
@@ -142,13 +339,7 @@
 
 
 
-//
 
-/*! @brief 检查微信是否已被用户安装
- *
- * @return 微信已安装返回YES，未安装返回NO。
- */
-//+(BOOL) isWXAppInstalled;
 
 
 
---
+++ XLMM/NSString+Encrypto.h
@@ -0,0 +1,15 @@
+//
+//  NSString+Encrypto.h
+//  XLMM
+//
+//  Created by younishijie on 15/9/26.
+//  Copyright © 2015年 上海己美. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Encrypto)
+
+- (NSString *) sha1; 
+
+@end
