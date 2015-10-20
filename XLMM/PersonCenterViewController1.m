b1a181237141958f86cf79fe04cdd95889b36df9
Mona <2444946401@qq.com>
2015-10-20 14:59:30 +0800

    待支付订单

--- XLMM/PersonCenterViewController1.m
+++ XLMM/PersonCenterViewController1.m
@@ -22,6 +22,7 @@
 }
 
 @property (nonatomic ,strong)NSArray *dataArray;
+@property (nonatomic, strong)NSMutableArray *labelArray;
 
 @end
 
@@ -47,7 +48,7 @@
     self.title = @"待支付订单";
     [self createNavigationBarWithTitle:@"待支付订单" selecotr:@selector(btnClicked:)];
     
-    
+    self.labelArray = [[NSMutableArray alloc] init];
    // [self createInfo];
     // Do any additional setup after loading the view from its nib.
     [self.collectionView registerClass:[ZhiFuCollectionCell class] forCellWithReuseIdentifier:kSimpleCellIdentifier];
@@ -81,8 +82,11 @@
     
     self.dataArray = [json objectForKey:@"results"];
     NSLog(@"dataArray = %@", self.dataArray);
+    
     self.collectionView.contentSize = CGSizeMake(SCREENWIDTH, 120*self.dataArray.count);
     [self.collectionView reloadData];
+    
+    [self createTimeLabels];
     
     
     
@@ -95,6 +99,17 @@
     theTimer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(timerFireMethod:) userInfo:self.dataArray repeats:YES];
     
     
+}
+- (void)createTimeLabels{
+    for (int i = 0; i<self.dataArray.count; i++) {
+       
+        UILabel * label = [[UILabel alloc]initWithFrame:CGRectMake(256, 8, 60, 15)];
+        label.textAlignment = NSTextAlignmentLeft;
+        label.font = [UIFont systemFontOfSize:12];
+        label.textColor = [UIColor colorWithR:229 G:49 B:120 alpha:1];
+        [self.labelArray addObject:label];
+    }
+    NSLog(@"label = %@", self.labelArray);
 }
 
 - (void)creatrDefaultView{
@@ -113,7 +128,7 @@
     UILabel *label1 = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 300, 30)];
     
     label1.font = [UIFont systemFontOfSize:18];
-    label1.text = @"亲，您的待支付还是空的";
+    label1.text = @"您的待支付还是空的";
     label1.textColor = [UIColor blackColor];
     label1.textAlignment = NSTextAlignmentCenter;
     label1.center = self.view.center;
@@ -250,18 +265,25 @@
     [string replaceCharactersInRange:range withString:@"/"];
     cell.createLabel.text = string;
     
-    int i = (int)indexPath.row;
-    shengyuTimeLabel[i] = [[UILabel alloc]initWithFrame:CGRectMake(256, 8, 60, 15)];
-    shengyuTimeLabel[i].textAlignment = NSTextAlignmentLeft;
-    shengyuTimeLabel[i].font = [UIFont systemFontOfSize:12];
-    shengyuTimeLabel[i].textColor = [UIColor colorWithR:229 G:49 B:120 alpha:1];
     
-    [cell.contentView addSubview:shengyuTimeLabel[i]];
+    for (int i = 0; i<self.labelArray.count; i++) {
+        UILabel *label = (UILabel *)[cell.contentView viewWithTag:i +100];
+        NSLog(@"label = %@", label);
+        [label removeFromSuperview];
+        
+    }
     
-    cell.timeLabel.text = shengyushijian;
+    
+     UILabel *label = (UILabel *)[self.labelArray objectAtIndex:indexPath.row];
+    NSLog(@"label = %@ and index.row = %ld", label, (long)indexPath.row);
+    label.tag = indexPath.row +100;
+    
+    [cell.contentView addSubview:label];
+    
+    
     
     cell.statusLabel.text = [dic objectForKey:@"status_display"];
-    cell.paymentLable.text = [NSString stringWithFormat:@"¥%@",[dic objectForKey:@"payment"]];
+    cell.paymentLable.text = [NSString stringWithFormat:@"¥%.1f",[[dic objectForKey:@"payment"] floatValue]];
     cell.idLabel.text = [dic objectForKey:@"tid"];
     
     return cell;
@@ -326,11 +348,12 @@
           NSDateComponents *d = [[NSCalendar currentCalendar] components:unitFlags fromDate:[NSDate date] toDate:endDate options:0];
 
         shengyushijian = [NSString stringWithFormat:@"%02ld:%02ld", (long)[d minute], (long)[d second]];
-        NSLog(@"shengyu shijian = %@" , shengyushijian);
-        if ([shengyushijian isEqualToString:@"00:00"]) {
+     //   NSLog(@"shengyu shijian = %@" , shengyushijian);
+        if ([d minute] < 0 || [d second] < 0) {
             shengyushijian = @"00:00";
         }
-        shengyuTimeLabel[i].text = shengyushijian;
+        UILabel *label = (UILabel *)[self.labelArray objectAtIndex:i];
+        label.text = shengyushijian;
     }
 
 }
--- XLMM/ZhiFuCollectionCell.h
+++ XLMM/ZhiFuCollectionCell.h
@@ -14,6 +14,7 @@
 @property (weak, nonatomic) IBOutlet UILabel *timeLabel;
 @property (weak, nonatomic) IBOutlet UILabel *idLabel;
 @property (weak, nonatomic) IBOutlet UILabel *statusLabel;
+@property (weak, nonatomic) IBOutlet UILabel *shengyuTime;
 
 @property (weak, nonatomic) IBOutlet UILabel *paymentLable;
 
