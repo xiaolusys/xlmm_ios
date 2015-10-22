8400e1a037bcbb7cf0f2e3359a0215db74de8dee
Mona <2444946401@qq.com>
2015-10-22 17:22:14 +0800

    10-22

---
+++ XLMM/NewLeftViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewLeftViewController.h
+//  XLMM
+//
+//  Created by younishijie on 15/10/22.
+//  Copyright © 2015年 上海己美. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewLeftViewController : UIViewController
+
+@end
---
+++ XLMM/SettingViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingViewController.h
+//  XLMM
+//
+//  Created by younishijie on 15/10/22.
+//  Copyright © 2015年 上海己美. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingViewController : UIViewController
+
+@end
