bca9041abf7b22f3215c9f1cafbbdab37e50cd38
ying.zhang <ying.zhang@xiaolu.so>
2016-01-03 13:14:25 +0800

    小鹿妈妈页面

---
+++ XLMM/MaMaChartTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MaMaChartTableViewCell.h
+//  XLMM
+//
+//  Created by 张迎 on 16/1/3.
+//  Copyright © 2016年 上海己美. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MaMaChartTableViewCell : UITableViewCell
+
+@end
---
+++ XLMM/MaMaOrderTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  MaMaOrderTableViewCell.m
+//  XLMM
+//
+//  Created by 张迎 on 16/1/3.
+//  Copyright © 2016年 上海己美. All rights reserved.
+//
+
+#import "MaMaOrderTableViewCell.h"
+
+@implementation MaMaOrderTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
