99bfb41fdf8cdec680f6c564c3244e055a8a0927
chongyang.chen <chongyang.chen@xiaolu.so>
2016-01-03 14:01:14 +0800

    妈妈首页

--- XLMM/MaMaCenterViewController.m
+++ XLMM/MaMaCenterViewController.m
@@ -13,7 +13,14 @@
 
 @end
 
-@implementation MaMaCenterViewController
+@implementation MaMaCenterViewController{
+    UIButton *backButton;
+    UILabel *jineLabel;
+    UILabel *levelLabel;
+    UILabel *jiluLabel;
+    UILabel *shouyiLabel;
+    
+}
 
 
 - (void)viewWillAppear:(BOOL)animated{
@@ -27,9 +34,31 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
     self.mamaTableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
-    UIView *headView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, 200)];
-    headView.backgroundColor = [UIColor lightGrayColor];
+    self.mamaTableView.backgroundColor = [UIColor colorWithRed:243/255.0 green:243/255.0 blue:244/255.0 alpha:1];
+    UIView *headView;
+    
+    NSArray *views = [[NSBundle mainBundle] loadNibNamed:@"MaMaHeadView" owner:nil options:nil];
+    headView = views[0];
+    headView.frame = CGRectMake(0, 0, self.view.bounds.size.width, 245);
+    self.mamaTableView.showsVerticalScrollIndicator = NO;
+    backButton = [headView viewWithTag:100];
+    levelLabel = [headView viewWithTag:200];
+    jineLabel = [headView viewWithTag:300];
+    jiluLabel = [headView viewWithTag:400];
+    shouyiLabel = [headView viewWithTag:500];
+    
+    [backButton addTarget:self action:@selector(backClicked:) forControlEvents:UIControlEventTouchUpInside];
+    
+    
+    levelLabel.text = @"12";
+    jineLabel.text = @"300";
+    jiluLabel.text = @"400";
+    shouyiLabel.text = @"500";
+    
+    
+    
     
     self.mamaTableView.tableHeaderView = headView;
     
@@ -39,7 +68,9 @@
     self.mamaTableView.dataSource = self;
     [self.view addSubview:self.mamaTableView];
 }
-
+- (void)backClicked:(UIButton *)button{
+    [self.navigationController popViewControllerAnimated:YES];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
