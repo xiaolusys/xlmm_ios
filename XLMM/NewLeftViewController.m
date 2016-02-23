a7228d5a500e5c067ad60764059463b10db123e7
chongyang.chen <chongyang.chen@xiaolu.so>
2016-02-23 16:08:13 +0800

    优化微信绑定手机号

--- XLMM/NewLeftViewController.m
+++ XLMM/NewLeftViewController.m
@@ -437,6 +437,7 @@
 
 - (IBAction)accountBtnAction:(id)sender {
 //    AccountViewController *account = [[AccountViewController alloc] initWithNibName:@"AccountViewController" bundle:nil];
+    return;
     
     if ([[NSUserDefaults standardUserDefaults] boolForKey:kIsLogin]) {
         AccountViewController *account = [[AccountViewController alloc] initWithNibName:@"AccountViewController" bundle:nil];
