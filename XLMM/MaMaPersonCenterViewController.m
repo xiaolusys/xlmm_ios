a019304a05fa40333684198d1529819a2a8d16df
chongyang.chen <chongyang.chen@xiaolu.so>
2016-03-05 11:34:32 +0800

    commit 隐藏应用导航栏

--- XLMM/MaMaPersonCenterViewController.m
+++ XLMM/MaMaPersonCenterViewController.m
@@ -73,7 +73,15 @@
 
 @implementation MaMaPersonCenterViewController
 
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    self.navigationController.navigationBarHidden = YES;
+}
 
+- (void)viewWillDisappear:(BOOL)animated{
+    [super viewWillDisappear:animated];
+    self.navigationController.navigationBarHidden = YES;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
