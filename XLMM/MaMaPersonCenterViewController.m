4fe75e70ab078252ac0ea8713b7f6943466bd690
chongyang.chen <chongyang.chen@xiaolu.so>
2016-03-05 16:38:01 +0800

    commit

--- XLMM/MaMaPersonCenterViewController.m
+++ XLMM/MaMaPersonCenterViewController.m
@@ -371,7 +371,7 @@
     self.dingdanLabel.text = [NSString stringWithFormat:@"%ld", (long)dingdanshu];
     self.shouyiLabel.text = [NSString stringWithFormat:@"%.2f", ticheng];
     
-    [self.mamaTableView reloadData];
+    //[self.mamaTableView reloadData];
 }
 
 
