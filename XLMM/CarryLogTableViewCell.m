69366797a6865859b0c4291095c2f669fb38921e
zhangying2015 <hnzyy@163.com>
2016-03-12 11:25:19 +0800

    历史收益优化

--- XLMM/CarryLogTableViewCell.m
+++ XLMM/CarryLogTableViewCell.m
@@ -33,35 +33,6 @@
 - (void)fillCarryModel:(CarryLogModel *)carryModel
                   type:(NSInteger)type {
     NSInteger carry_type = [carryModel.carry_type integerValue];
-//    
-//    if (carry_type == 1) {
-//        //直荐奖励
-//        self.photoImageV.image = [UIImage imageNamed:@"mamajiang"];
-//    }else if (carry_type == 2) {
-//        //团队奖励
-//        self.photoImageV.image = [UIImage imageNamed:@"mamatuan"];
-//    }else if ([carryModel.log_type isEqualToString:@"recruit"]){
-//        //奖金
-//        self.photoImageV.image = [UIImage imageNamed:@"mamajiang"];
-//    }else if ([carryModel.log_type isEqualToString:@"subsidy"]){
-//        //提成
-//        self.photoImageV.image = [UIImage imageNamed:@"mamati"];
-//    }else if ([carryModel.log_type isEqualToString:@"thousand"] || [carryModel.log_type isEqualToString:@"activity"]){
-//        //千元提成和参加活动收益
-//        self.photoImageV.image = [UIImage imageNamed:@"mamajiang"];
-//    }else if ([carryModel.log_type isEqualToString:@"ordred"]) {
-//        //订单红包
-//        self.photoImageV.image = [UIImage imageNamed:@"mamajiang"];
-//    }else if ([carryModel.log_type isEqualToString:@"buy"]){
-//        //支出
-//        self.photoImageV.image = [UIImage imageNamed:@"mamazhi"];
-//    }else if ([carryModel.log_type isEqualToString:@"fans_carry"]){
-//        //粉丝购买
-//        self.photoImageV.image = [UIImage imageNamed:@"mamafens"];
-//    }else if ([carryModel.log_type isEqualToString:@"group_bonus"]){
-//        //团队新增成员奖金
-//        self.photoImageV.image = [UIImage imageNamed:@"mamatuan"];
-//    } 
 
     if (type == 0) {
         //全部
@@ -77,28 +48,13 @@
         }
     }else if(type == 1) {
         //佣金
-        if (carry_type == 1) {
-            //直接订单
-            self.photoImageV.image = [UIImage imageNamed:@"mamayong"];
-        }else if (carry_type == 2) {
-            //粉丝订单
-            self.photoImageV.image = [UIImage imageNamed:@"mamafens"];
-        }else if(carry_type == 3) {
-            //下属订单
-            self.photoImageV.image = [UIImage imageNamed:@"mamayong"];
-        }
+        self.photoImageV.image = [UIImage imageNamed:@"mamayong"];
     }else if(type == 2) {
         //点击
         self.photoImageV.image = [UIImage imageNamed:@"mamafan"];
         
     }else if(type == 3) {
-        if (carry_type == 1) {
-            //直荐奖励
-            self.photoImageV.image = [UIImage imageNamed:@"mamajiang"];
-        }else if (carry_type == 2) {
-            //团队奖励
-            self.photoImageV.image = [UIImage imageNamed:@"mamajiang"];
-        }
+        self.photoImageV.image = [UIImage imageNamed:@"mamajiang"];
     }
 //    if ([carryModel.log_type isEqualToString:@"rebeta"]) {
 //        //佣金
@@ -138,8 +94,6 @@
     self.moneyLabel.text = [NSString stringWithFormat:@"+%.2f", [carryModel.carry_value floatValue]];
     self.sourceLabel.text = carryModel.carry_type_name;
     self.desLabel.text = @"哈哈哈，测试。。。";
-    
-    
 }
 
 @end
