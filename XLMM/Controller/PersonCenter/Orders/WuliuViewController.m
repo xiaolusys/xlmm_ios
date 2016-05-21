e339c247b596194fe02cbbaff2a5ae6b88f61271
wulei <crackwl@163.com>
2016-05-21 16:05:02 +0800

    物流信息展示

--- XLMM/Controller/PersonCenter/Orders/WuliuViewController.m
+++ XLMM/Controller/PersonCenter/Orders/WuliuViewController.m
@@ -11,17 +11,26 @@
 #import "MMClass.h"
 #import "UIColor+RGBColor.h"
 #import "SVProgressHUD.h"
+#import "LogisticsCollectionViewCell.h"
+#import "AFNetworking.h"
 
 @interface WuliuViewController ()
-
+@property (nonatomic, strong) NSArray *infoArray;
 @end
 
 @implementation WuliuViewController
+
+static NSString * const reuseIdentifier = @"LogisticsCell";
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     [self createNavigationBarWithTitle:@"物流信息" selecotr:@selector(goback)];
+    self.wuliuInfoChainView.backgroundColor = [UIColor backgroundlightGrayColor];
+    self.wuliuInfoChainView.delegate = self;
+    self.wuliuInfoChainView.dataSource = self;
+    self.wuliuInfoChainView.showsVerticalScrollIndicator = FALSE;
+    [self.wuliuInfoChainView registerClass:[LogisticsCollectionViewCell class] forCellWithReuseIdentifier:reuseIdentifier];
     
     [self getWuliuInfoFromServer];
 }
@@ -47,22 +56,35 @@
     NSLog(@"%@", urlString);
     
     
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:urlString]];
-        [self performSelectorOnMainThread:@selector(fetchedWuliuData:)withObject:data waitUntilDone:YES];
-    });
+//    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+//        NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:urlString]];
+//        [self performSelectorOnMainThread:@selector(fetchedWuliuData:)withObject:data waitUntilDone:YES];
+//    });
     
+    [SVProgressHUD showWithStatus:@"获取物流信息"];
+    AFHTTPRequestOperationManager *manage = [AFHTTPRequestOperationManager manager];
+    [manage GET:urlString parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        [SVProgressHUD dismiss];
+        if(responseObject == nil) return;
+        NSDictionary *info = responseObject;
+        [self fetchedWuliuData:info];
+        [self.wuliuInfoChainView reloadData ];
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        [SVProgressHUD dismiss];
+        NSLog(@"wuliu info get failed.");
+    }];
+
     
   }
 
-- (void)fetchedWuliuData:(NSData *)responseData{
+- (void)fetchedWuliuData:(NSDictionary *)responseData{
    // NSLog(@"%@",responseData);
     if (responseData == nil) {
         return;
     }
     
     
-    NSDictionary *dicJson = [NSJSONSerialization JSONObjectWithData:responseData options:kNilOptions error:nil];
+    NSDictionary *dicJson = responseData;
     NSLog(@"json = %@", dicJson);
     
     
@@ -70,33 +92,33 @@
     self.wuliuMiandanId.text = [dicJson objectForKey:@"order"];
     
  
-        NSArray *infoArray = [dicJson objectForKey:@"data"];
-        NSInteger length = infoArray.count;
+    self.infoArray = [dicJson objectForKey:@"data"];
+    NSInteger length = self.infoArray.count;
     if (length == 0) {
-        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"温馨提示" message:@"您的订单暂无物流信息，请稍候查询" delegate:nil cancelButtonTitle:@"确定" otherButtonTitles:nil];
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"温馨提示" message:@"您的订单暂未查询到物流信息，请稍候查询或到快递公司网站查询" delegate:nil cancelButtonTitle:@"确定" otherButtonTitles:nil];
         [alertView show];
     }
         
-        if (length > 0) {
-                NSDictionary *lastWuliuInfo = [infoArray firstObject];
-                NSString *timeText = [lastWuliuInfo objectForKey:@"time"];
-                timeText = [self spaceFormatTimeString:timeText];
-                
-                NSString *infoText = [lastWuliuInfo objectForKey:@"content"];
-                [self displayLastWuliuInfoWithTime: timeText andInfo:infoText];
-        }
-        NSInteger MAX = 4; // we only display at most 3 wuliu info.
-        if (length < MAX) {
-            MAX = length;
-        }
-        for (int i=1; i<MAX; ++i) {
-            NSDictionary *wuliuInfo =  infoArray[i];
-            NSString *timeText = [wuliuInfo objectForKey:@"time"];
-            timeText = [self spaceFormatTimeString:timeText];
-            
-            NSString *infoText = [wuliuInfo objectForKey:@"content"];
-            [self displayWuliuInfoWithOrder:i andTime:timeText andInfo:infoText];
-        }
+//        if (length > 0) {
+//                NSDictionary *lastWuliuInfo = [self.infoArray firstObject];
+//                NSString *timeText = [lastWuliuInfo objectForKey:@"time"];
+//                timeText = [self spaceFormatTimeString:timeText];
+//                
+//                NSString *infoText = [lastWuliuInfo objectForKey:@"content"];
+//                [self displayLastWuliuInfoWithTime: timeText andInfo:infoText];
+//        }
+//        NSInteger MAX = 4; // we only display at most 3 wuliu info.
+//        if (length < MAX) {
+//            MAX = length;
+//        }
+//        for (int i=1; i<MAX; ++i) {
+//            NSDictionary *wuliuInfo =  self.infoArray[i];
+//            NSString *timeText = [wuliuInfo objectForKey:@"time"];
+//            timeText = [self spaceFormatTimeString:timeText];
+//            
+//            NSString *infoText = [wuliuInfo objectForKey:@"content"];
+//            [self displayWuliuInfoWithOrder:i andTime:timeText andInfo:infoText];
+//        }
    
     
 
@@ -130,7 +152,7 @@
 }
 
 
--(void)displayWuliuInfoWithOrder:(NSInteger)order andTime:(NSString*)timeText andInfo:(NSString*)infoText{
+-(void)displayWuliuInfoWithOrder:(UICollectionViewCell *)cell andTime:(NSString*)timeText andInfo:(NSString*)infoText{
     NSArray *views = [[NSBundle mainBundle] loadNibNamed:@"WuliuInfoView" owner:nil options:nil];
     UIView *view = views[0];
     UIView *circleView = [view viewWithTag:100];
@@ -147,10 +169,8 @@
     circleView.layer.cornerRadius = 4.5;
     circleView.backgroundColor = normalColor;
     
-    NSInteger originY = 17 + order*72;
-    
-    view.frame = CGRectMake(0, originY, SCREENWIDTH, 80);
-    [self.wuliuInfoChainView addSubview:view];
+    view.frame = CGRectMake(0, 0, SCREENWIDTH, 80);
+    [cell addSubview:view];
     
     
 }
@@ -164,5 +184,34 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+#pragma mark <UICollectionViewDataSource>
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+    
+    return 1;
+}
+
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    
+    //    return 0;
+    return self.infoArray.count;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    LogisticsCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:reuseIdentifier forIndexPath:indexPath];
+    
+    NSDictionary *wuliuInfo =  [self.infoArray objectAtIndex:indexPath.row];
+    NSString *timeText = [wuliuInfo objectForKey:@"time"];
+    timeText = [self spaceFormatTimeString:timeText];
+    
+    NSString *infoText = [wuliuInfo objectForKey:@"content"];
+    [self displayWuliuInfoWithOrder:cell andTime:timeText andInfo:infoText];
+    
+//    [cell fillCellWithData:[self.infoArray objectAtIndex:indexPath.row]];
+    
+    return cell;
+}
 
 @end
