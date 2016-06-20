d7e55a30e74c574b8ae4f54aa987defabe64199e
renshuai.cui <pieceboom@163.com>
2016-06-20 19:08:05 +0800

    购物车导航栏

--- XLMM/Controller/Carts/PurchaseViewController1.m
+++ XLMM/Controller/Carts/PurchaseViewController1.m
@@ -158,12 +158,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
-
-//    self.navigationController.navigationBar.tintColor = [UIColor whiteColor];
-//    self.navigationController.navigationBar.barStyle = UIBarStyleBlackOpaque;
-
-    self.edgesForExtendedLayout = UIRectEdgeNone;
     
     [self createNavigationBarWithTitle:@"确认订单" selecotr:@selector(backButtonClicked:)];
     self.addressViewWidth.constant = SCREENWIDTH;
