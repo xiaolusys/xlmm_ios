bd143e0fab8d846ddfa4d3916f9aa70fddd7a851
wulei <crackwl@163.com>
2016-07-31 18:41:00 +0800

    mipish new version

--- XLMM/Venders/QiniuSDK/Http/QNSessionManager.m
+++ XLMM/Venders/QiniuSDK/Http/QNSessionManager.m
@@ -171,7 +171,7 @@ static BOOL needRetry(NSHTTPURLResponse *httpResponse, NSError *error) {
         request.URL = url;
         domain = url.host;
     } else if (_noProxy && _dns != nil && [url.scheme isEqualToString:@"http"]) {
-        if (isIpV6FullySupported() || ![QNIP isV6]) {
+        if (isIpV6FullySupported()) {
             ips = [_dns queryWithDomain:[[QNDomain alloc] init:domain hostsFirst:NO hasCname:YES maxTtl:1000]];
             double duration = [[NSDate date] timeIntervalSinceDate:startTime];
 
